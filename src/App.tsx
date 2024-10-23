1e976067e3177c85f657bca85004ba40c0cb5c21
yuyang2505 <ongyuyang2505@gmail.com>
2024-10-24 00:25:57 +0800

    login and register component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import LoginRegister from './components/login/login-register'
 
 
 function App () {
   return (
-    <div>
-
+    <div className='flex justify-center items-center h-screen'>
+      <LoginRegister />
     </div>
   )
 }
